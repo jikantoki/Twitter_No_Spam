ec877ba4977cff3551dfcb2cef9e140fee7f53b3
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-01-29 15:57:42 +0900

    色々な機能を追加

--- options.js
+++ options.js
@@ -1,26 +1,50 @@
 /** オプションの項目 */
 const options = {
   whitelist: undefined,
+  blacklist: undefined,
+  blackwordlist: undefined,
   viewRemovedAccountInfo: true,
 }
 
 /** 読み込み時に動く処理 */
 document.addEventListener('DOMContentLoaded', async () => {
   await chrome.storage.sync.get(
-    ['whitelist', 'viewRemovedAccountInfo'],
+    ['whitelist', 'blacklist', 'blackwordlist', 'viewRemovedAccountInfo'],
     (item) => {
       options.whitelist = item.whitelist
+      options.blacklist = item.blacklist
+      options.blackwordlist = item.blackwordlist
       options.viewRemovedAccountInfo = item.viewRemovedAccountInfo
 
       if (options.whitelist) {
         document.getElementById('option-whitelist-textarea').value =
           options.whitelist
       }
+      if (options.blacklist) {
+        document.getElementById('option-blacklist-textarea').value =
+          options.blacklist
+      }
+      if (options.blackwordlist) {
+        document.getElementById('option-blackwordlist-textarea').value =
+          options.blackwordlist
+      }
+      if (options.blackwordlist === false || options.blackwordlist === '') {
+        document.getElementById('option-blackwordlist-textarea').value = ''
+      }
       if (!options.viewRemovedAccountInfo) {
-        document.getElementById('option-dataview-checkbox').checked = false
+        if (options.viewRemovedAccountInfo === false) {
+          document.getElementById('option-dataview-checkbox').checked = false
+        }
       }
     }
   )
+
+  /** オプションフォーム内エンターキーで保存 */
+  document.getElementById('option').addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      document.getElementById('option-save-button').click()
+    }
+  })
 
   /** 保存時の処理 */
   document
@@ -30,6 +54,14 @@ document.addEventListener('DOMContentLoaded', async () => {
       const whitelist = document.getElementById(
         'option-whitelist-textarea'
       ).value
+      /** ブラックリストの項目 */
+      const blacklist = document.getElementById(
+        'option-blacklist-textarea'
+      ).value
+      /** ブラックワードリストの項目 */
+      const blackwordlist = document.getElementById(
+        'option-blackwordlist-textarea'
+      ).value
       /** 非表示にしたアカウントの情報を表示するか？ */
       const viewRemovedAccountInfo = document.getElementById(
         'option-dataview-checkbox'
@@ -37,6 +69,8 @@ document.addEventListener('DOMContentLoaded', async () => {
       chrome.storage.sync.set(
         {
           whitelist: whitelist,
+          blacklist: blacklist,
+          blackwordlist: blackwordlist,
           viewRemovedAccountInfo: viewRemovedAccountInfo,
         },
         function () {}
--- twitterNoSpam.js
+++ twitterNoSpam.js
@@ -22,23 +22,34 @@ const isJapanese = (string) => {
 /** オプションの項目 */
 const options = {
   whitelist: [],
+  blacklist: [],
+  blackwordlist: ['プロフみて', 'プロフ見て'],
   viewRemovedAccountInfo: true,
 }
 /** セットアップ完了時にtrueにする */
 let setuped = false
 
 /** 読み込み時に動く処理 */
-chrome.storage.sync.get(['whitelist', 'viewRemovedAccountInfo'], (item) => {
-  if (item.whitelist) {
-    options.whitelist = item.whitelist.split(',')
-  }
-  options.viewRemovedAccountInfo = item.viewRemovedAccountInfo
-  if (options.viewRemovedAccountInfo === undefined) {
-    options.viewRemovedAccountInfo = true
-  }
+chrome.storage.sync.get(
+  ['whitelist', 'blacklist', 'blackwordlist', 'viewRemovedAccountInfo'],
+  (item) => {
+    if (item.whitelist) {
+      options.whitelist = item.whitelist.split(',')
+    }
+    if (item.blackwordlist || item.blackwordlist === undefined) {
+      options.blackwordlist = item.blackwordlist.split(',')
+    }
+    if (item.blacklist) {
+      options.blacklist = item.blacklist.split(',')
+    }
+    options.viewRemovedAccountInfo = item.viewRemovedAccountInfo
+    if (options.viewRemovedAccountInfo === undefined) {
+      options.viewRemovedAccountInfo = true
+    }
 
-  setuped = true
-})
+    setuped = true
+  }
+)
 
 /** Welcomeメッセージ */
 console.log('Twitter No Spam is active')
@@ -50,7 +61,10 @@ setInterval(() => {
     return
   }
   /** URLでリプ欄が表示される場所かを判定 */
-  if (!location.pathname.includes('/status/')) {
+  if (
+    !location.pathname.includes('/status/') &&
+    !location.pathname.includes('/search')
+  ) {
     return
   }
   const sections = document.querySelectorAll('section>div>div')
@@ -66,7 +80,39 @@ setInterval(() => {
         const isPost = item.children[0].children[0]
         if (isPost && isPost.querySelector('article')) {
           /** ポスト内に含まれるaタグ */
-          let aTags = isPost.querySelector('article').querySelectorAll('a')
+          const aTags = isPost.querySelector('article').querySelectorAll('a')
+          /** ミュートしているアカウントによるポストです。の文字 */
+          const alreadyMuted = isPost.querySelector(
+            'article>div>div>div>div>div>div>div>span>span>span'
+          )
+          if (
+            alreadyMuted &&
+            alreadyMuted.innerHTML ===
+              'ミュートしているアカウントによるポストです。'
+          ) {
+            /** スパムタグが付いていない場合に、スパムタグを追加 */
+            if (
+              !isPost.classList.contains('is-spam') &&
+              !isPost.classList.contains('is-not-spam')
+            ) {
+              /** スパムタグを付ける */
+              isPost.classList.add('is-spam')
+
+              /** オプションで有効になっている場合は、スパムを削除したことを表示 */
+              if (options.viewRemovedAccountInfo) {
+                const spamRemovedDiv = document.createElement('div')
+                const spamRemovedText = document.createTextNode(
+                  `ミュートしているアカウントによるポストです`
+                )
+                spamRemovedDiv.appendChild(spamRemovedText)
+                spamRemovedDiv.classList.add('spam-removed')
+                isPost.before(spamRemovedDiv)
+              }
+              continue
+            }
+          }
+
+          let aTagCount = 0
           for (aTag of aTags) {
             /** ポストのユーザー名 */
             let username
@@ -76,12 +122,20 @@ setInterval(() => {
             let isJapaneseName
             /** 認証バッチはあるか？ */
             let isOfficial
+            /** ツイートの本文 */
+            let tweetText = ''
+            /** 絵文字またはURLのみのツイートならTrueになる */
+            let isBlackText = false
+            /** 非推奨ワードがツイートに含まれていたらTrueになる */
+            let hasBlackWord = false
 
             /** アカウント名解析 */
             let spans = aTag.querySelector('div>div>span>span')
             if (spans && !spans.children[0]) {
               username = spans.innerHTML
               isJapaneseName = isJapanese(username)
+            } else {
+              continue
             }
 
             /** 公式アカウント察知用 */
@@ -93,22 +147,101 @@ setInterval(() => {
               }
             }
 
-            if (!isJapaneseName && isOfficial) {
+            for (a of aTags) {
+              if (
+                a.querySelector('div>span') &&
+                !a.querySelector('div>span').children[0]
+              ) {
+                id = a.querySelector('div>span').innerHTML
+                break
+              }
+            }
+
+            /** ブラックリスト判定フラグ */
+            let isBlack = false
+            for (blackId of options.blacklist) {
+              if (blackId === id) {
+                isBlack = true
+              }
+            }
+
+            /** ツイートの中身を取得する用 */
+            const tweetDivs = isPost.querySelectorAll(
+              'article>div>div>div>div>div>div'
+            )
+            if (
+              tweetDivs &&
+              !aTagCount &&
+              !isPost.classList.contains('is-not-spam-text') &&
+              !isPost.classList.contains('is-spam-text')
+            ) {
+              for (tweetDiv of tweetDivs) {
+                if (tweetDiv.children) {
+                  for (tweetSpan of tweetDiv.children) {
+                    if (
+                      tweetSpan.tagName !== 'DIV' &&
+                      tweetSpan.tagName !== 'ARTICLE' &&
+                      tweetSpan.tagName !== 'G'
+                    ) {
+                      if (tweetSpan.tagName === 'IMG') {
+                        tweetText += tweetSpan.getAttribute('alt')
+                      } else {
+                        tweetText += tweetSpan.innerText
+                      }
+                    }
+                  }
+                  if (tweetText !== '') {
+                    break
+                  }
+                }
+                if (tweetText !== '') {
+                  break
+                }
+              }
+
+              /** 絵文字の文字コード */
+              const regEmoji = new RegExp(
+                /[\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]/,
+                'g'
+              )
+              /** 絵文字削除 */
+              const removeEmoji = (input) =>
+                // 絵文字を空文字('')に置き換える
+                input.replace(regEmoji, '')
+
+              tweetText = removeEmoji(tweetText)
+
+              /** ブラックワードリストの単語が含まれてないか照合 */
+              for (blackWord of options.blackwordlist) {
+                if (tweetText.includes(blackWord)) {
+                  hasBlackWord = true
+                }
+              }
+              if (
+                tweetText !== '' &&
+                !URL.canParse(tweetText) &&
+                !hasBlackWord
+              ) {
+                /** スパムじゃないよタグを付ける */
+                isPost.classList.add('is-not-spam-text')
+              } else {
+                /** スパムタグを付ける */
+                isPost.classList.add('is-spam-text')
+                isBlackText = true
+              }
+            }
+
+            /** ブラックリスト判定時には、usernameがtrueかどうかの判定が必要 */
+            if (
+              (!isJapaneseName && isOfficial) ||
+              (username && isBlack) ||
+              isBlackText
+            ) {
               /** スパムタグが付いていない場合に、スパムタグを追加 */
               if (
                 !isPost.classList.contains('is-spam') &&
                 !isPost.classList.contains('is-not-spam')
               ) {
-                for (a of aTags) {
-                  if (
-                    a.querySelector('div>span') &&
-                    !a.querySelector('div>span').children[0]
-                  ) {
-                    id = a.querySelector('div>span').innerHTML
-                    break
-                  }
-                }
-
                 let continueFlag = false
                 for (whiteId of options.whitelist) {
                   if (whiteId === id) {
@@ -124,7 +257,15 @@ setInterval(() => {
                   continue
                 }
 
-                console.log(`${username}(${id})はアフィカスかも？`)
+                if (isBlack) {
+                  console.log(`${username}(${id})はブラックリストです`)
+                } else {
+                  if (isBlackText) {
+                    console.log(`${username}(${id})のリプは品質が低いです`)
+                  } else {
+                    console.log(`${username}(${id})はアフィカスかも？`)
+                  }
+                }
 
                 /** スパムタグを付ける */
                 isPost.classList.add('is-spam')
@@ -133,7 +274,11 @@ setInterval(() => {
                 if (options.viewRemovedAccountInfo) {
                   const spamRemovedDiv = document.createElement('div')
                   const spamRemovedText = document.createTextNode(
-                    `${username}(${id})のリプを非表示にしました！`
+                    isBlack
+                      ? `${username}(${id})はブラックリストに登録されています`
+                      : isBlackText
+                      ? `${username}(${id})のリプは品質が低いです`
+                      : `${username}(${id})のリプを非表示にしました！`
                   )
                   spamRemovedDiv.appendChild(spamRemovedText)
                   spamRemovedDiv.classList.add('spam-removed')
@@ -141,6 +286,7 @@ setInterval(() => {
                 }
               }
             }
+            aTagCount++
           }
         }
       }
